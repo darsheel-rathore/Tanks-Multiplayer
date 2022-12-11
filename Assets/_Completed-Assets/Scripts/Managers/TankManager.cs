f77159bd807597e13767f6bcc0a864634f78412e
IBX00 <ibx.05n@gmail.com>
2022-12-11 23:57:24 +0530

    Shooting synced. MPTankShooting created.
    MPShellExplosion created though not used.

--- Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
+++ Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
@@ -20,7 +20,7 @@ namespace Complete
         
 
         private TankMovement m_Movement;                        // Reference to tank's movement script, used to disable and enable control.
-        private TankShooting m_Shooting;                        // Reference to tank's shooting script, used to disable and enable control.
+        private MPTankShooting m_Shooting;                        // Reference to tank's shooting script, used to disable and enable control.
         private GameObject m_CanvasGameObject;                  // Used to disable the world space UI during the Starting and Ending phases of each round.
 
 
@@ -28,7 +28,7 @@ namespace Complete
         {
             // Get references to the components.
             m_Movement = m_Instance.GetComponent<TankMovement> ();
-            m_Shooting = m_Instance.GetComponent<TankShooting> ();
+            m_Shooting = m_Instance.GetComponent<MPTankShooting> ();
             m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas> ().gameObject;
 
             // Set the player numbers to be consistent across the scripts.
